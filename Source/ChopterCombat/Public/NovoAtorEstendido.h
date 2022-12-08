ae0df310d962f17573d9820c0ea980a5d9ebaec3
GeorgeNasPires <georgenasciment@hotmail.com>
2022-12-08 04:51:45 -0300

    v1.0

---
+++ Source/ChopterCombat/Public/NovoAtorEstendido.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractiveItem.h"
+#include "NovoAtorEstendido.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHOPTERCOMBAT_API ANovoAtorEstendido : public AInteractiveItem
+{
+	GENERATED_BODY()
+	
+};
